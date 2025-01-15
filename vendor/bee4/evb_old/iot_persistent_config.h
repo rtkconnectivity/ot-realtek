06b3b8e48ba7ec9325a00ef132e985005a68065e
yushuo_ko <yushuo_ko@realtek.com>
2025-01-15 14:57:29 +0800

    Remove unnecessary files and update the SDK

--- vendor/bee4/evb_old/iot_persistent_config.h
+++
@@ -1,8 +0,0 @@
-#include "flash_map.h"
-
-#define ZIGBEE_PER_ADDR                 BKP_DATA1_ADDR
-#define ZIGBEE_PER_SIZE                 0x00000000
-#define THREAD_PER_ADDR                 FTL_ADDR + 0x0000E000
-#define THREAD_PER_SIZE                 0x00002000
-#define KVS_ADDR                        FTL_ADDR
-#define KVS_SIZE                        0x0000E000
--- vendor/bee4/rtl8771gtv/iot_persistent_config.h
+++
@@ -1,4 +0,0 @@
-#include "flash_map.h"
-
-#define THREAD_PER_ADDR                 BKP_DATA1_ADDR
-#define THREAD_PER_SIZE                 0x00000000
